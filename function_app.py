e773962863e522966fb0cb010e8fd48d970c1b8e
eccfi <64589974+rhcproc@users.noreply.github.com>
2026-06-08 01:52:04 +1200

    feat: add a bronze functions

--- function_app.py
+++ function_app.py
@@ -2,13 +2,14 @@ import logging
 
 import azure.functions as func
 
-from main import run_scheduled_task
+# from main import run_scheduled_task
+from etl.bronze import run_scheduled_task as run_bronze_task
 
 app = func.FunctionApp()
 
 
 @app.timer_trigger(
-    schedule="0 * * * * *",
+    schedule="0 */2 * * * *",
     arg_name="timer",
     run_on_startup=False,
     use_monitor=True,
@@ -17,5 +18,22 @@ def simple_writer(timer: func.TimerRequest) -> None:
     if timer.past_due:
         logging.warning("simple-writer timer trigger is past due.")
 
-    result = run_scheduled_task()
+    result = run_bronze_task()
     logging.info(result)
+
+
+# Function 2
+# @app.blob_trigger(
+#     arg_name="inputblob",
+#     path="bronze/crypto/{name}",
+#     connection="AzureWebJobsStorage"
+# )
+# def bronze_to_silver(inputblob):
+#     ...
+#     save_to_silver(...)
+
+
+if __name__ == "__main__":
+    bronze_result = run_bronze_task()
+    logging.info(bronze_result)
+    # print(bronze_result)
